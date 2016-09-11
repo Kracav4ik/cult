a587814bfb72a5b1fc05fb9a620a4263e6eb435f
Kracav4ik <dikama2013@yandex.ru>
2016-09-12 00:14:28 +0300

    speedups

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,25 @@ FILE* out;
 class UniversalMachine{
 private:
     uint32_t registers[8];
+    uint32_t free_index;
     uint32_t exec_finger;
-    std::unordered_map<uint32_t, std::vector<uint32_t> > arrays;
+    typedef std::vector<uint32_t> ArrType;
+    std::unordered_map<uint32_t, ArrType*> arrays;
 
     uint32_t create_array(uint32_t size){
-        uint32_t index = 1;
-        while (true){
-            if (arrays.count(index) == 0){
-                arrays[index] = std::vector<uint32_t>(size);
-                return index;
-            }
-            index++;
-        }
+        uint32_t index = free_index;
+        arrays[index] = new ArrType(size);
+        free_index++;
+        return index;
    }
     void delete_array(uint32_t index){
+        delete arrays[index];
         arrays.erase(index);
     }
 public:
-    UniversalMachine(const char* file)
-            :exec_finger(0)
+    UniversalMachine(const char* file) :
+            exec_finger(0),
+            free_index(0)
     {
         for (int i = 0; i < 8; i++){
             registers[i] = 0;
@@ -42,63 +42,79 @@ public:
             uint32_t x = (p[0] << 24) + (p[1] << 16) + (p[2] << 8) + p[3];
             program.push_back(x);
         }
-        arrays[0] = program;
+        create_array(program.size());
+        *arrays[0] = program;
         fclose(f);
     }
 
     void run(){
         while (true){
-            uint32_t word = arrays[0][exec_finger];
+            uint32_t word = (*arrays[0])[exec_finger];
             exec_finger++;
             const int opcode = word>>28;
             const int A = (word >> 6) & 7;
             const int B = (word >> 3) & 7;
             const int C = (word >> 0) & 7;
-            if(opcode==0){
-                if(registers[C]){
-                    registers[A] = registers[B];
+            switch (opcode) {
+                case 0:
+                    if(registers[C]){
+                        registers[A] = registers[B];
+                    }
+                    break;
+                case 1:
+                    registers[A] = (*arrays[registers[B]])[registers[C]];
+                    break;
+                case 2:
+                    (*arrays[registers[A]])[registers[B]] = registers[C];
+                    break;
+                case 3:
+                    registers[A] = registers[B] + registers[C];
+                    break;
+                case 4:
+                    registers[A] = registers[B] * registers[C];
+                    break;
+                case 5:
+                    registers[A] = registers[B] / registers[C];
+                    break;
+                case 6:
+                    registers[A] = ~(registers[B] & registers[C]);
+                    break;
+                case 7:
+                    return; // HALT
+                case 8:
+                    registers[B] = create_array(registers[C]);
+                    break;
+                case 9:
+                    delete_array(registers[C]);
+                    break;
+                case 10:
+                    fprintf(out, "%c", char(registers[C]));
+                    fflush(out);
+                    break;
+                case 11:
+                    registers[C] = (uint32_t) getchar();
+                    break;
+                case 12:
+                    *arrays[0] = *arrays[registers[B]];
+                    exec_finger = registers[C];
+                    break;
+                case 13: {
+                    const int D = (word>>25) & 7;
+                    const uint32_t value = word & ((1 << 25) - 1);
+                    registers[D] = value;
                 }
-            } else if(opcode==1){
-                registers[A] = arrays[registers[B]][registers[C]];
-            } else if(opcode==2){
-                arrays[registers[A]][registers[B]] = registers[C];
-            } else if(opcode==3){
-                registers[A] = registers[B] + registers[C];
-            } else if(opcode==4){
-                registers[A] = registers[B] * registers[C];
-            } else if(opcode==5){
-                registers[A] = registers[B] / registers[C];
-            } else if(opcode==6){
-                registers[A] = ~(registers[B] & registers[C]);
-            } else if(opcode==7){
-                return;
-            } else if(opcode==8){
-                registers[B] = create_array(registers[C]);
-            } else if(opcode==9){
-                delete_array(registers[C]);
-            } else if(opcode==10){
-                fprintf(out, "%c", char(registers[C]));
-                fflush(out);
-            } else if(opcode==11){
-                registers[C] = (uint32_t) getchar();
-            } else if(opcode==12){
-                arrays[0] = arrays[registers[B]];
-                exec_finger = registers[C];
-            } else if(opcode==13){
-                const int D = (word>>25) & 7;
-                const uint32_t value = word & ((1 << 25) - 1);
-                registers[D] = value;
-            } else{
-                printf("INVALID OPCODE %d FROM WORD %x\n",opcode,word);
-                return;
+                    break;
+                default:
+                    printf("INVALID OPCODE %d FROM WORD %x\n",opcode,word);
+                    return;
             }
         }
     }
 };
 
 int main(int argc, const char* argv[]) {
-//    const char* file = "sandmark.umz";
-    const char* file = "codex.umz";
+    const char* file = "sandmark.umz";
+//    const char* file = "codex.umz";
 //    const char* file = "out.um";
     if (argc > 1) {
         file = argv[1];
